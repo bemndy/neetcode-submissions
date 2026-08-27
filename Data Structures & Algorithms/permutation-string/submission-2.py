3523e175e00bd050738d1d39a047b0c8481bded1
brandon 🍋 <brandn.martinez5@gmail.com>
2026-08-27 13:09:26 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        h1, h2 = {}, {}
+        for ch in s1:
+            h1[ch] = 1 + h1.get(ch, 0)
+        l = 0
+        r = len(s1)
+        while r < len(s2)+1:
+            curr = s2[l:r]
+            # print(curr)
+            for ch in curr:
+                h2[ch] = 1 + h2.get(ch, 0)
+            # print(h2, h1)
+
+            if h2 != h1:
+                h2 = {}
+                r += 1
+                l += 1
+            else:
+                return True
+
+        return False
+            
+
